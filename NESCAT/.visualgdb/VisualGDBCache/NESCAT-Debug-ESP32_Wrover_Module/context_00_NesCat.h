d616d485c0c362840b04779c966d5e8f1b02cae1
Bas Visscher <bas.visscher24@gmail.com>
2022-01-22 12:15:20 +0100

    Removed buildfiles

--- NESCAT/.visualgdb/VisualGDBCache/NESCAT-Debug-ESP32_Wrover_Module/context_00_NesCat.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "C:/WSHome/NesCat/NESCAT/.visualgdb/VisualGDBCache/NESCAT-Debug-ESP32_Wrover_Module/GeneratedPrototypes.h"
-
--- NESCAT/Output/ESP32_Wrover_Module/Debug/sketch/README.md
+++
@@ -1,5 +0,0 @@
-#line 1 "C:\\WSHome\\NesCat\\Arduino\\README.md"
-
-Let see if this:
-https://github.com/nathalis/NesCat
-Works on the WT32-SC01
